cd482371512c63f85416df3c2c0fb132b97a488e
MahmoudSayed216 <mahmoud_das@outlook.com>
2025-08-25 19:41:30 +0300

    d

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ class CringeNet(nn.Module):
             p.requires_grad = False
 
         for name, param in self.backbone.named_parameters():
-            if "7" in name:
+            if "7" in name or "6" in name:
                 param.requires_grad = True
 
 
